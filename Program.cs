5da5544947906440fed89e2fb2dd490022d8ca9c
b3n1d0rm <98117230+b3n1d0rm@users.noreply.github.com>
2022-04-06 19:08:25 +0300

    Add files via upload
    commit 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array1 = new string[5] {"414", "87", "Ky ky", "Yeah", "Bro"};
 string[] array2 = new string[array1.Length];
+void bkb(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
